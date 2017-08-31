856f404cdc0125b67614c63411a4b6cdf96366cd
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-08-31 15:08:47 -0500

    set up first example test to see if I get test to perform

---
+++ dice-roll/test/main.test.js
@@ -0,0 +1,8 @@
+// testing randpm stuff based on Joe's code along
+describe('Just a test', () => {
+  it('should be equal', () => {
+    let expected = 3;
+    let actual = 1 + 2;
+    equal(expected, actual);
+  });
+});
